8f7cece3205efc970e5d912522da36b6ca97c758
Priyank Patel <pri4216@Knights.ucf.edu>
2018-07-08 18:44:23 -0400

    Added New Project For Leaderboard web app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+        
+
+
+    }
+}
